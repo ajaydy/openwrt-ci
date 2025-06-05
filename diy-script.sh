fd01cd99830f5578a5aedd31761ad0d9c642e29e
ajaydy <mohdjamilafiq@gmail.com>
2025-06-05 15:53:11 +0800

    fix - add homeproxy

--- diy-script.sh
+++ diy-script.sh
@@ -5,7 +5,7 @@ set -e
 sed -i '/src-git luci/d' feeds.conf.default
 
 # Add OUI feed if not already present
-grep -q 'src-git oui' feeds.conf.default || echo "src-git oui https://github.com/zhaojh329/oui.git" >> feeds.conf.default
+grep -q 'src-git oui' feeds.conf.default || echo "src-git oui https://github.com/ajaydy/oui.git" >> feeds.conf.default
 
 # Remove any existing feeds/luci folder
 rm -rf feeds/luci
